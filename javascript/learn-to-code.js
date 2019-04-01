23e0926c14b57aad386c668dfedfa02d767a9db1
El-Glory <elglore@El-Glory.PC>
2019-04-01 22:47:35 +0100

    My first commit

---
+++ javascript/learn-to-code.js
@@ -0,0 +1,74 @@
+// var name = "Jack";
+// var age = 23;
+
+// var message = "Hi, my name is " + name +  " and  I am " + age +  " years old ";
+
+// console.log(message);
+
+
+
+// var firstName = "John";
+// var lastName = "JacobJingleHeimerSmith";
+// var dateOfBirth = "10-09-82";
+// var age = 23;
+// var profileImgUrl = 'http://coolpicks.com/johnjacob.jpg';
+
+// var loginWelcomeMessage = "Welcome, " + firstName + ". Happy " + age + "rd birthday!" ;
+
+// console.log(loginWelcomeMessage);
+
+
+// var myAccountBalance = 300;
+// var nikeSBShoes = 799.26;
+
+// if(nikeSBShoes <= myAccountBalance){
+//      myAccountBalance -= nikeSBShoes;
+//      console.log('We just bought some dope shoes!');
+//      console.log("Account balance: " + myAccountBalance );
+// }else{
+//     console.log('Shift away son!');
+// }
+
+
+// var phone = 100000;
+// var gadgets = 14000;
+// var transport = 1500;
+// var moneyAvailable = 110000;
+// var purse = true;
+// var totalCostOfMoney = phone + gadgets + transport;
+
+// var accessories = phone + gadgets;
+
+// if(purse === true){
+//    console.log("You can make transactions");
+// }else{
+//     console.log('Money no reach');
+// }
+
+
+// if(accessories >= moneyAvailable){
+//     moneyAvailable -= accessories;
+//     console.log('purshase completed');
+//     console.log('Balance: ' + moneyAvailable);
+// }else if((moneyAvailable  >= accessories) || purse){
+//     console.log("You have enough cash to make the transaction");
+// }
+// else{
+//     console.log('Oops,not enough cash!');
+// }
+
+//array
+// var student = ["Timmy", "Jane", "Toad"];
+
+// var naughtyList = [];
+// naughtyList.push(student[0]);
+
+// //removing element from an array
+// var index = naughtyList.indexOf("Timmy");
+// console.log(naughtyList);
+// if(index > -1){
+//     naughtyList.splice(index,1);
+// }
+
+// console.log(naughtyList);
+
